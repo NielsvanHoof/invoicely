ed8b92b400a6d5daa20b460eaa3f96add078030e
Niels van Hoof <vanhoofniels15@gmail.com>
2025-05-02 15:43:21 +0200

    route fix

--- routes/invoices/routes.php
+++ routes/invoices/routes.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Invoices\InvoiceDestroyController;
 use App\Http\Controllers\Invoices\InvoiceDownloadFileController;
 use App\Http\Controllers\Invoices\InvoiceEditController;
 use App\Http\Controllers\Invoices\InvoiceIndexController;
+use App\Http\Controllers\Invoices\InvoiceShowController;
 use App\Http\Controllers\Invoices\InvoiceStoreController;
 use App\Http\Controllers\Invoices\InvoiceUpdateController;
-use App\Http\Controllers\Invoices\InvoiceShowController;
 
 Route::group([
     'prefix' => 'invoices',
